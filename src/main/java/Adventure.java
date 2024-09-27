1dc11c0bea51093d4f65ce083970c4846f9fdee0
CadenFinley <caden.finley0789@gmail.com>
2024-09-27 16:32:20 -0500

    Add more scenes

--- src/main/java/Adventure.java
+++ src/main/java/Adventure.java
@@ -10,27 +10,49 @@ import java.io.Console;
  * Albert Tucker
  * Grijesh Shrestha
  */
- class Adventure {
-        public static void main(String[] args) throws InterruptedException {
+class Adventure {
+    private Console console = System.console();
+    private String command;
+
+    public static void main(String[] args) throws InterruptedException {
         TextEngine.printWithDelays("Adventure V1, by BarrettHall:: Albert Tucker, Caden Finley, and Grijesh Shrestha");
+        Adventure adventure = new Adventure();
+        adventure.inAmerica();
+    }
 
-        TextEngine.printWithDelays("You are in a lucious, green area with trees, not Abilene.\nWhat is your command: north or exit?");
+    private void inAmerica() throws InterruptedException{
+        while(true){
+            TextEngine.printWithDelays("You are in a lucious, green area with trees, not Abilene.\nWhat is your command: north or exit?");
+            command = console.readLine();
+            if (command.equals("north")) {
+                inCanada();
+                break;
+            } else if (command.equals("exit")) {
+                TextEngine.printWithDelays("See ya next time!");
+                System.exit(0);
+        
+            } else {
+                TextEngine.printWithDelays("I'm sorry, I don't understand that command.");
+            }
+        }
+    }
 
-	    Console console = System.console();
-        String command = console.readLine();
-
-        if (command.equals("north")) {
-            TextEngine.printWithDelays("Eh, you are in Canada. Good luck with all the mooses. Bye for now.");
-            System.exit(0);
-	    
-        } else if (command.equals("exit")) {
-            TextEngine.printWithDelays("See ya next time!");
-            System.exit(0);
-	    
-        } else {
-            // how did the user arrive at this branch?
-            // TODO: be more useful to the user
-            System.exit(0);
+    private void inCanada() throws InterruptedException{
+        while(true){
+            TextEngine.printWithDelays("Eh, you are in Canada. Good luck with all the mooses.\nWhat is your command: north, south or exit?");
+            command = console.readLine();
+            if (command.equals("north")) {
+                TextEngine.printWithDelays("Eh, you are on the North Pole. Good luck cold, hope you brought your coat. brrrrrrr...");
+                System.exit(0);
+            } else if (command.equals("south")) {
+                inAmerica();
+                break;
+            } else if (command.equals("exit")) {
+                TextEngine.printWithDelays("See ya next time!");
+                System.exit(0);
+            }else {
+                TextEngine.printWithDelays("I'm sorry, I don't understand that command.");
+            }
         }
     }
 }
