cf1b4d4afc6c86cd0db423507929d58e022c3e38
CadenFinley <caden.finley0789@gmail.com>
2024-09-29 23:25:49 -0500

    added more enemies
    changed how enemies are instanciated

--- src/main/java/Enemy.java
+++ src/main/java/Enemy.java
@@ -12,13 +12,38 @@ public class Enemy {
     public final static Console console = System.console();
     public static String command;
     public static String ignore;
-    private static final Map<String, Integer> enemyDamageValues= Map.of( //damage values for each enemy
-            "goblin", 5,
-            "orc", 10, 
-            "troll", 15,
-            "dragon", 20,
-            "bandit", 3
-    );
+    private static final Map<String, Integer> enemyDamageValues = Map.copyOf(Map.ofEntries( //damage values for each enemy
+            Map.entry("goblin", 5),
+            Map.entry("orc", 10),
+            Map.entry("troll", 15),
+            Map.entry("dragon", 20),
+            Map.entry("bandit", 3),
+            Map.entry("skeleton", 5),
+            Map.entry("zombie", 7),
+            Map.entry("ghost", 10),
+            Map.entry("demon", 15),
+            Map.entry("vampire", 20),
+            Map.entry("werewolf", 25),
+            Map.entry("witch", 30),
+            Map.entry("giant", 35),
+            Map.entry("slime", 2),
+            Map.entry("mimic", 5),
+            Map.entry("golem", 10),
+            Map.entry("elemental", 15),
+            Map.entry("wyvern", 20),
+            Map.entry("hydra", 25),
+            Map.entry("kraken", 30),
+            Map.entry("phoenix", 35),
+            Map.entry("chimera", 40),
+            Map.entry("minotaur", 45),
+            Map.entry("cyclops", 50),
+            Map.entry("medusa", 55),
+            Map.entry("sphinx", 60),
+            Map.entry("leviathan", 65),
+            Map.entry("behemoth", 70),
+            Map.entry("tiamat", 75),
+            Map.entry("fenrir", 80)
+    ));
     public static int spawnEnemy(String type,int quantity) throws InterruptedException { //return the total damage as negative int so that you can change health
         if(quantity > 1) {
             TextEngine.printWithDelays("You fight the " + type + "s!", false);
