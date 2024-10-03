c67316b783b7c8a928f9f83469363b49bf549752
CadenFinley <caden.finley0789@gmail.com>
2024-10-03 17:45:30 -0500

    fixed bug item in dungeon, removed extranious run and fight option in dungeons
    added function hasItemIndeingeon that returns boolean to check wheater or not player took the item

--- src/main/java/DesertPlainsDungeon.java
+++ src/main/java/DesertPlainsDungeon.java
@@ -55,46 +55,38 @@ public class DesertPlainsDungeon extends Dungeon {
         if (desertPlainsDungeon[currentPlayerPosition[0]][currentPlayerPosition[1]] == 2 && roomsBeenTo[currentPlayerPosition[0]][currentPlayerPosition[1]] == 0) {
             if (foundItemRooms < items.size()) {
                 String randomItem = items.get(rand.nextInt(items.size()));
-                Room.hasItemInRoom(randomItem, 1);
-                items.remove(randomItem);
+                if (hasItemInDungeon(randomItem, 1)) {
+                    items.remove(randomItem);
+                    lastPosition = currentPlayerPosition.clone();
+                    roomsBeenTo[currentPlayerPosition[0]][currentPlayerPosition[1]] = desertPlainsDungeon[currentPlayerPosition[0]][currentPlayerPosition[1]];
+                    foundItemRooms++;
+                } else {
+                    currentPlayerPosition = lastPosition.clone();
+                    save = currentPlayerPosition.clone();
+                    Main.loadSave();
+                }
+            } else {
+                TextEngine.printWithDelays("You have already found all the items in this dungeon", false);
                 lastPosition = currentPlayerPosition.clone();
                 roomsBeenTo[currentPlayerPosition[0]][currentPlayerPosition[1]] = desertPlainsDungeon[currentPlayerPosition[0]][currentPlayerPosition[1]];
-                foundItemRooms++;
                 Main.loadSave();
             }
         }
         if (desertPlainsDungeon[currentPlayerPosition[0]][currentPlayerPosition[1]] == 3 && roomsBeenTo[currentPlayerPosition[0]][currentPlayerPosition[1]] == 0) {
-            Room.hasItemInRoom("heart container", 1);
-            lastPosition = currentPlayerPosition.clone();
-            roomsBeenTo[currentPlayerPosition[0]][currentPlayerPosition[1]] = desertPlainsDungeon[currentPlayerPosition[0]][currentPlayerPosition[1]];
-            Main.loadSave();
+            if (hasItemInDungeon("heart container", 1)) {
+                lastPosition = currentPlayerPosition.clone();
+                roomsBeenTo[currentPlayerPosition[0]][currentPlayerPosition[1]] = desertPlainsDungeon[currentPlayerPosition[0]][currentPlayerPosition[1]];
+                foundItemRooms++;
+            } else {
+                currentPlayerPosition = lastPosition.clone();
+                save = currentPlayerPosition.clone();
+                Main.loadSave();
+            }
         }
         if (desertPlainsDungeon[currentPlayerPosition[0]][currentPlayerPosition[1]] == 1 && roomsBeenTo[currentPlayerPosition[0]][currentPlayerPosition[1]] == 0) {
-            TextEngine.printWithDelays("You have entered a room with enemies and were ambushed!", false);
-            if (!Player.autoFight) {
-                TextEngine.printWithDelays("What is your command (run or fight)?", true);
-                while (true) {
-                    ignore = Room.console.readLine();
-                    command = Room.console.readLine();
-                    switch (command.toLowerCase()) {
-                        case "run" -> {
-                            TextEngine.printWithDelays("You ran away from the enemies", false);
-                            currentPlayerPosition = lastPosition.clone(); // Restore the last position
-                            save = currentPlayerPosition.clone();
-                            TextEngine.enterToNext();
-                            Main.loadSave();
-                        }
-                        case "fight" -> {
-                            fightRandomEnemies(5);
-                        }
-                        default -> {
-                            Dungeon.defaultDungeonArgs(command.toLowerCase());
-                        }
-                    }
-                }
-            } else {
-                fightRandomEnemies(5);
-            }
+
+            fightRandomEnemies(5);
+
         }
         if (desertPlainsDungeon[currentPlayerPosition[0]][currentPlayerPosition[1]] == 4 && roomsBeenTo[currentPlayerPosition[0]][currentPlayerPosition[1]] == 0) {
             TextEngine.printWithDelays("You have entered a room with a mini boss", false);
@@ -251,13 +243,14 @@ public class DesertPlainsDungeon extends Dungeon {
     public static void fightRandomEnemies(int number) throws InterruptedException {
         int numberOfEnemies = rand.nextInt(number);
         if (numberOfEnemies == 0) {
-            TextEngine.printWithDelays("The enemies got frightened and ran off!", false);
+            TextEngine.printWithDelays("There were no enemies in this room", false);
             TextEngine.enterToNext();
             lastPosition = currentPlayerPosition.clone();
             roomsBeenTo[currentPlayerPosition[0]][currentPlayerPosition[1]] = desertPlainsDungeon[currentPlayerPosition[0]][currentPlayerPosition[1]];
             Main.loadSave();
             return;
         }
+        TextEngine.printWithDelays("You have entered a room with enemies and were ambushed!", false);
         String enemyType = enemies.get(rand.nextInt(enemies.size()));
         Player.changeHealth(Enemy.spawnEnemy(enemyType, numberOfEnemies));
         lastPosition = currentPlayerPosition.clone();
