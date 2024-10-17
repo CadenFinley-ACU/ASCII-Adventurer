b7676f79504894307b50354f4ca7b67db92b0121
CadenFinley <147097345+CadenFinley@users.noreply.github.com>
2024-10-16 21:27:06 -0500

    adjusted map icons and layout

--- src/main/java/DungeonGenerator.java
+++ src/main/java/DungeonGenerator.java
@@ -398,7 +398,7 @@ public class DungeonGenerator {
                             System.out.print(yellowColor + "[P] " + resetColor);
                         } else if (isAdjacent(i, j, passedPosition)) {
                             switch (passedMatrix[i][j]) {
-                                case 2, 5, 4 ->
+                                case 2, 5 ->
                                     System.out.print(greenColor + "[?] " + resetColor);
                                 case 8 ->
                                     System.out.print(redColor + "[B] " + resetColor);
@@ -406,6 +406,8 @@ public class DungeonGenerator {
                                     System.out.print("[S] ");
                                 case 6 ->
                                     System.out.print(greenColor + "[$] " + resetColor);
+                                case 4 ->
+                                    System.out.print(redColor + "[!] " + resetColor);
                                 default ->
                                     System.out.print("[~] ");
                             }
@@ -419,6 +421,8 @@ public class DungeonGenerator {
                                     System.out.print(redColor + "[B] " + resetColor); // Special marker for boss room
                                 case 9 ->
                                     System.out.print("[S] "); // Special marker for start room
+                                case 4 ->
+                                    System.out.print(redColor + "[!] " + resetColor); // Special marker for mini boss room
                                 default ->
                                     System.out.print("[■] "); // Default case for other values
                             }
@@ -443,18 +447,39 @@ public class DungeonGenerator {
                             System.out.print(yellowColor + "[P] " + resetColor);
                         } else {
                             switch (passedMatrix[i][j]) {
-                                case 2, 3, 5 ->
-                                    System.out.print(greenColor + "[?] " + resetColor);
+                                case 2, 3, 5 -> {
+                                    if (unlocked[i][j] > 0) {
+                                        System.out.print("[■] ");
+                                    } else {
+                                        System.out.print(greenColor + "[?] " + resetColor);
+                                    }
+                                }
                                 case 8 ->
                                     System.out.print(redColor + "[B] " + resetColor);
-                                case 4 ->
-                                    System.out.print(redColor + "[!] " + resetColor);
-                                case 9 ->
+                                case 4 -> {
+                                    if (unlocked[i][j] > 0) {
+                                        System.out.print("[■] ");
+                                    } else {
+                                        System.out.print(redColor + "[!] " + resetColor);
+                                    }
+                                }
+                                case 9 -> {
                                     System.out.print("[S] ");
-                                case 6 ->
-                                    System.out.print(greenColor + "[$] " + resetColor);
-                                default ->
-                                    System.out.print("[ ] ");
+                                }
+                                case 6 -> {
+                                    if (unlocked[i][j] > 0) {
+                                        System.out.print("[■] ");
+                                    } else {
+                                        System.out.print(greenColor + "[$] " + resetColor);
+                                    }
+                                }
+                                case 1 -> {
+                                    if (unlocked[i][j] > 0) {
+                                        System.out.print("[■] ");
+                                    } else {
+                                        System.out.print("[ ] ");
+                                    }
+                                }
                             }
                         }
                     } else {
