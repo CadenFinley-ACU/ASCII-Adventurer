727882f476be03f44c8bc6c43a9202ec73049dca
CadenFinley <147097345+CadenFinley@users.noreply.github.com>
2024-10-28 14:21:21 -0500

    speed

--- src/main/java/Player.java
+++ src/main/java/Player.java
@@ -1060,6 +1060,7 @@ public class Player {
                         minDistance = distance;
                         closestDungeonX = x;
                         closestDungeonY = y;
+                        break;
                     }
                 }
             }
